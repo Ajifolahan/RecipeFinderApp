0c530a88d8cbe9af3be9710c673186e12006ef61
cakeller2 <cakeller2@quinnipiac.edu>
2023-03-21 11:05:57 -0400

    SplashScreen implemented

---
+++ app/src/main/java/edu/quinnipiac/edu/ser210/RecpieFinderApp/SplashScreen.kt
@@ -0,0 +1,27 @@
+package edu.quinnipiac.edu.ser210.RecpieFinderApp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+class SplashScreen : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_splash_screen,container,false)
+        //variable for start button
+        val startButton = view.findViewById<Button>(R.id.start_button)
+
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_splashScreen_to_secondFragment)
+        }
+
+        return view
+    }
+}
